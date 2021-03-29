fc691b8d1ad8a141fe8c2cf99cfaa6fcbedb634e
tavolafourcade <tavolafourcade@gmail.com>
2021-03-28 19:38:58 -0500

    Added the file selector for image on product-create-form.ejs

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,6 +1,7 @@
 // ************ Require's ************
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 
 // ************ Controller Require ************
 const productsController = require('../controllers/productsController');
